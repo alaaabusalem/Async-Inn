fbbee3135d500feec5ad5cf44af11d9d7b1c3856
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-07-19 19:21:54 +0300

    finishing lab 12

---
+++ Async-Inn/Async-Inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Async_Inn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn.Data
+{
+	public class AsyncInnDbContext:DbContext
+	{
+        public AsyncInnDbContext(DbContextOptions options) :base(options)
+        {
+            
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<Amenity> Amenities { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Hotel>().HasData(
+					new Hotel() { Id = 1, Name = "Async Inn Amman", City = "Amman", Country = "Jordan", StreetAddress = " 12-12-12", State = "Amman", Phone = "12345" },
+			new Hotel() { Id = 2, Name = "Async Inn Aqaba", City = "Aqaba", Country = "Jordan", StreetAddress = " 13-13-13", State = "Aqaba", Phone = "67890" },
+			new Hotel() { Id = 3, Name = "Async Inn Irbid", City = "Irbid", Country = "Jordan", StreetAddress = " 14-14-14", State = "Irbid", Phone = "56437" }
+				);
+			modelBuilder.Entity<Room>().HasData(
+				new Room() {Id=1, Name= "Royalty" },
+				new Room() { Id = 2, Name = "Golden" },
+				new Room() { Id = 3, Name = "Silver" }
+				);
+
+			modelBuilder.Entity<Amenity>().HasData(
+				new Amenity() { Id = 1, Name = "Coffee Maker" },
+				new Amenity() { Id = 2, Name = "Overlooking the sea" },
+				new Amenity() { Id = 3, Name = "Air Conditioner\r\n" }
+				);
+		}
+	}
+}
